6a19fec337516aa1ef31aed9938b1c60035e20c6
Qureshi Abdul Kader Javed <81419817+AbdulkaderJaved-SoftwareEngineer@users.noreply.github.com>
2025-01-31 15:18:37 +0530

    1/31/2025 3:18 PM

--- src/components/NavbarTop.jsx
+++ src/components/NavbarTop.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import {React,useState} from 'react'
 import { NavbarText } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useRef } from 'react';
-
+import styled from 'styled-components';
 
 function NavbarTop({ onSkillsClick,onContactClick }) {
+  const [isHovered1, setIsHovered1] = useState(false);
+  const [isHovered2, setIsHovered2] = useState(false);
   
-  
+  const buttonStyle = {
+    borderBottom: isHovered1 ? "3px solid white" : "transparent",
+    transition: isHovered1 ? "ease-in-out 0.1s":"",
+    color:"white"
+    
+  };
+
+  const buttonStyle2 = {
+    borderBottom: isHovered2 ? "3px solid white" : "transparent",
+    transition: isHovered2 ? "ease-in-out 0.1s":"",
+    color:"white"
+    
+  };
   return (
 
 <>
@@ -29,12 +43,25 @@ function NavbarTop({ onSkillsClick,onContactClick }) {
     
       <Navbar.Brand style={{color:'white'}}>Abdul Kader Javed Qureshi</Navbar.Brand>
       
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Toggle aria-controls="basic-navbar-nav" style={{
+
+color: "white",
+backgroundColor: "white"
+      }}/>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
           
-          <Nav.Link onClick={onSkillsClick}>Skills</Nav.Link>
-          <Nav.Link onClick={onContactClick}>Contact Me</Nav.Link>
+          <Nav.Link onClick={onSkillsClick} style={buttonStyle}
+          onMouseEnter={() => setIsHovered1(true)}
+          onMouseLeave={() => setIsHovered1(false)}>
+            Skills
+          </Nav.Link>
+          <Nav.Link onClick={onContactClick} style={buttonStyle2}
+          onMouseEnter={() => setIsHovered2(true)}
+          onMouseLeave={() => setIsHovered2(false)}
+          
+          
+          >Contact Me</Nav.Link>
          
           
           
