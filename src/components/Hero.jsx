6bbd0b112dd73a6d204b69f808f04b13f45cac0e
Qureshi Abdul Kader Javed <81419817+AbdulkaderJaved-SoftwareEngineer@users.noreply.github.com>
2025-02-24 14:15:30 +0530

    02/24/2025 2:18 PM

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ import RightImage from './RightImage';
 import LeftText from './LeftText';
 import ConfettiExplosion from 'react-confetti-explosion';
 
-import pdf from '/files/Qureshi_Abdul_Kader_Javed_ResumeV-36.pdf'
+import pdf from '/files/Qureshi Abdul Kader Javed ResumeV-39.pdf'
 
 
 function Hero() {
@@ -44,7 +44,7 @@ function Hero() {
     
           const link = document.createElement("a");
           link.href = pdfPath; // Path to the PDF in the public folder
-          link.download = "Qureshi_Abdul_Kader_Javed_ResumeV-36.pdf"; // File name for download
+          link.download = "Qureshi Abdul Kader Javed ResumeV-39.pdf"; // File name for download
           
           link.click();
           
